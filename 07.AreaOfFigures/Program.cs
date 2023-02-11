43bebd24aa4a0450a6f05f8e5363295677fb5fa8
icakabby98 <82100504+icakabby98@users.noreply.github.com>
2023-02-12 01:22:17 +0200

    Add project files.

---
+++ 07.AreaOfFigures/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+
+namespace _07.AreaOfFigures
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           string figure = Console.ReadLine();
+
+            if (figure == "square")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                Console.WriteLine(sideA * sideA);
+            }
+            else if (figure == "rectangle")
+            {
+                double sideA = double.Parse((Console.ReadLine()));
+                double sideB = double.Parse((Console.ReadLine()));
+                Console.WriteLine(sideA * sideB);   
+
+            }
+            else if (figure == "circle")
+            {
+                double radians = double.Parse((Console.ReadLine()));
+                Console.WriteLine(Math.PI * Math.Pow(radians, 2));
+            }
+            else if (figure == "triangle")
+            {
+                double sideA = double.Parse(Console.ReadLine());
+                double higth = double.Parse((Console.ReadLine()));
+                Console.WriteLine((sideA * higth) / 2 );
+            }
+
+        }
+    }
+}
---
+++ Music/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Globalization;
+
+namespace Music
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+           double num = double.Parse(Console.ReadLine());
+            if (num >= 5.50) 
+            {
+                Console.WriteLine("Excellent!");
+            }
+
+        }
+    }
+}
